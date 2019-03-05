8dd61effde4e83194d8e60c04be77b01c6a79d12
Golabe <2643552374@qq.com>
2019-03-05 11:24:42 +0800

    基本完成

--- app/src/main/java/com/github/golabe/network/MainActivity.java
+++ app/src/main/java/com/github/golabe/network/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.golabe.network;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.github.golabe.networklib.NetworkManager;
 import com.github.golabe.networklib.annotaion.Network;
@@ -24,23 +25,29 @@ public class MainActivity extends AppCompatActivity {
         switch (netType) {
             case AUTO:
                 Log.d(TAG, "network: 有网络");
+                toast(" 有网络");
                 break;
             case WIFI:
                 Log.d(TAG, "network: WIFI网络");
+                toast(" WIFI网络");
                 break;
             case MOBILE:
-                Log.d(TAG, "network: 移动网络");
+                Log.d(TAG, "network: MOBILE网络");
+                toast(" WIFI网络");
                 break;
             case NONE:
                 Log.d(TAG, "network: 没网络");
+                toast(" 没网络");
                 break;
         }
     }
 
+    public void toast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         NetworkManager.getDefault().unregister(this);
-        NetworkManager.getDefault().unregisterAll();
     }
 }
